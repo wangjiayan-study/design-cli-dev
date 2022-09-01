d5f23b643dc5355c186f55d9575340062d62fe02
wangjiayan <765390818@qq.com>
2022-09-01 10:46:49 +0800

    使用yargs

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -1,2 +1,20 @@
 #!/usr/bin/env node 
-console.log('@design-cli-dev/code')
+const lib = require('@design-cli-dev/utils')
+const yargs = require('yargs/yargs')
+// 得到参数
+const { hideBin } = require('yargs/helpers')
+const arg = hideBin(process.argv)
+
+yargs(arg).
+usage('Usage: $0 <command> [options]')
+.demandCommand(1, "A command is required. Pass --help to see all available commands and options.")
+.command('init','初始化',function (yargs) {
+    return yargs.option('u', {
+      alias: 'url',
+      describe: 'the URL to make an HTTP request to'
+    })
+  },function (argv) {
+    console.log(argv.url)
+  }
+)
+.argv

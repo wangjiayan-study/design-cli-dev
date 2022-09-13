8682143164a5915dd79954b355db1031363a4a9f
wangjiayan <765390818@qq.com>
2022-09-13 08:31:40 +0800

    请求后端模板数据

--- commands/create/lib/create.js
+++ commands/create/lib/create.js
@@ -1,129 +1,208 @@
-'use strict';
-const Command = require('@design-cli-dev/command')
-const fs = require('node:fs')
-const inquirer = require('inquirer');
-const fse = require('fs-extra')
-const log = require('@design-cli-dev/logs')
-const semver = require('semver')
-
-const TYPE_PROJECT = 'project';
-const TYPE_COMPONENT = 'component';
+"use strict";
+const Command = require("@design-cli-dev/command");
+const fs = require("node:fs");
+const inquirer = require("inquirer");
+const fse = require("fs-extra");
+const log = require("@design-cli-dev/logs");
+const semver = require("semver");
+const getProjectTemplate = require("./getProjectTemplate");
+const { verbose } = require("@design-cli-dev/logs");
+const TYPE_PROJECT = "project";
+const TYPE_COMPONENT = "component";
 class Create extends Command {
-    constructor (args){
-        super(args)
+  init() {
+    this.projectName = this._argv[0] || "";
+    this.force = !!this._cmd.force;
+    log.verbose("projectName", this.projectName);
+    log.verbose("force", this.force);
+  }
+  async exec() {
+    try {
+      // 1. 准备阶段
+      await this.prepare();
+      // 2. 下载模板
+    } catch (err) {
+      throw new Error(err);
     }
-    init () {
-        console.log('init方法')
+  }
+  /**
+   * 准备阶段
+   */
+  async prepare() {
+    // 0. 判断项目模板是否存在
+    const { data: template } = await getProjectTemplate();
+    if (!template || template.length === 0) {
+      throw new Error("项目模板不存在");
     }
-    async exec () {
-        try{
-
-     
-        await this.prepare()
-    } catch(err){
-        throw new Error(err)
-    }
-    }
-    /**
-     * 准备阶段
-     */
-    async prepare(){
     // 1. 判断当前目录是否为空
+    this.template = template;
     if (this.isCwdEmpty()) {
-        // 1.1 询问是否继续创建
-        const {isContinue} = await inquirer.prompt([{
-            type:'confirm',
-            message:'当前目录不为空,是否清空当前目录继续创建？',
-            name:'isContinue'
-        }])
-        if (isContinue){
-            fse.emptyDir(process.cwd())
-        }
+      // 1.1 询问是否继续创建
+      const { isContinue } = await inquirer.prompt([
+        {
+          type: "confirm",
+          message: "当前目录不为空,是否清空当前目录继续创建？",
+          name: "isContinue",
+        },
+      ]);
+      if (isContinue) {
+        fse.emptyDir(process.cwd());
+      }
     }
     // 3. 选择创建项目或组件
-    this.getProjectInfo()
+    return this.getProjectInfo();
+  }
 
-    // 4. 获取项目的基本信息
+  async getProjectInfo() {
+    this.projectInfo = {};
+    // 1. 选择创建项目或组件
+    const type = await this._getProjectType();
+    this.template = this.template.filter((template) =>
+      template.tag.includes(type)
+    );
+    this.projectPrompt = [];
+    this._validProjectName(type);
+    const title = type === TYPE_PROJECT ? "项目" : "组件";
+    this.projectPrompt.push(
+      {
+        type: "input",
+        name: "projectVersion",
+        message: `请输入${title}版本号`,
+        default: "1.0.0",
+        validate: function (v) {
+          const done = this.async();
+          setTimeout(function () {
+            if (!!!semver.valid(v)) {
+              done("请输入合法的版本号");
+              return;
+            }
+            done(null, true);
+          }, 0);
+        },
+        filter: function (v) {
+          if (!!semver.valid(v)) {
+            return semver.valid(v);
+          } else {
+            return v;
+          }
+        },
+      },
+      {
+        type: "list",
+        name: "projectTemplate",
+        message: `请选择${title}模板`,
+        choices: this._createTemplateChoice(),
+      }
+    );
+    if (type === TYPE_PROJECT) {
+      // 2. 获取项目的基本信息
+      const project = await inquirer.prompt(this.projectPrompt);
+      this.projectInfo = {
+        ...this.projectInfo,
+        type,
+        ...project,
+      };
+    } else if (type === TYPE_COMPONENT) {
+      const descriptionPrompt = {
+        type: "input",
+        name: "componentDescription",
+        message: "请输入组件描述信息",
+        default: "",
+        validate: function (v) {
+          const done = this.async();
+          setTimeout(function () {
+            if (!v) {
+              done("请输入组件描述信息");
+              return;
+            }
+            done(null, true);
+          }, 0);
+        },
+      };
+      this.projectPrompt.push(descriptionPrompt);
+      // 2. 获取组件的基本信息
+      const component = await inquirer.prompt(this.projectPrompt);
+      this.projectInfo = {
+        ...this.projectInfo,
+        type,
+        ...component,
+      };
     }
-    async getProjectInfo (){
-          // 1. 选择创建项目或组件
-          const { type } = await inquirer.prompt({
-            type: 'list',
-            name: 'type',
-            message: '请选择初始化类型',
-            default: TYPE_PROJECT,
-            choices: [{
-                name: '项目',
-                value: TYPE_PROJECT
-            }, {
-                name: '组件',
-                value: TYPE_COMPONENT
-            }]
-        })
-        log.verbose('type', type)
-        if (type === TYPE_PROJECT) {
-            const o = await inquirer.prompt([{
-                type: 'input',
-                name: 'projectName',
-                message: '请输入项目名称',
-                default: '',
-                validate: function (v) {
-                    const done = this.async();
-
-                    setTimeout(function () {
-                        // 1. 输入的首字符必须为英文字母
-                        // 2. 尾字符必须为英文或数字，不能为字符
-                        // 3. 字符允许"-_"
-                        // 合法: a, a-b, a_b, a-b-c, a-b1-c1,a_b1_c1a1,a1,a1-b1-c1, a1_b1_c1
-                        // 不合法: 1,a_,a-.a_1,a-1
-                        const reg = /^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/
-                        if (!reg.test(v)) {
-                            done('请输入合法的项目名称');
-                            return;
-                        }
-                        done(null, true);
-                    }, 0);
-                },
-                filter: function (v) {
-                    return v
-                }
-            }, {
-                type: 'input',
-                name: 'projectVersion',
-                message: '请输入项目版本号',
-                default: '1.0.0',
-                validate: function (v) {
-                    const done = this.async();
-                    setTimeout(function () {
-                        if (!(!!semver.valid(v))) {
-                            done('请输入合法的版本号');
-                            return;
-                        }
-                        done(null, true);
-                    }, 0);
-                },
-                filter: function (v) {
-                    if (semver.valid(v)) {
-                        return semver.valid(v)
-                    } else {
-                        return v
-                    }
-                }
-            }])
-    
-        }
+    return this.projectInfo;
+  }
+  async _getProjectType() {
+    const { type } = await inquirer.prompt({
+      type: "list",
+      name: "type",
+      message: "请选择初始化类型",
+      default: TYPE_PROJECT,
+      choices: [
+        {
+          name: "项目",
+          value: TYPE_PROJECT,
+        },
+        {
+          name: "组件",
+          value: TYPE_COMPONENT,
+        },
+      ],
+    });
+    log.verbose("type", type);
+    return type;
+  }
+  _createTemplateChoice() {
+    return this.template.map((item) => ({
+      value: item.npmName,
+      name: item.name,
+    }));
+  }
+  _validProjectName(type) {
+    function isValidName(v) {
+      return /^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(
+        v
+      );
     }
-    isCwdEmpty(){
-      const localPath = process.cwd()
-      const fileList = fs.readdirSync(localPath)
-      return !!fileList?.length 
+    const title = type === TYPE_PROJECT ? "项目" : "组件";
+    const projectNamePrompt = {
+      type: "input",
+      name: "projectName",
+      message: `请输入${title}名称`,
+      default: "",
+      validate: function (v) {
+        const done = this.async();
+        setTimeout(function () {
+          // 1.首字符必须为英文字符
+          // 2.尾字符必须为英文或数字，不能为字符
+          // 3.字符仅允许"-_"
+          if (!isValidName(v)) {
+            done(`请输入合法的${title}名称`);
+            return;
+          }
+          done(null, true);
+        }, 0);
+      },
+      filter: function (v) {
+        return v;
+      },
+    };
+    let isProjectNameValid = false;
+    if (isValidName(this.projectName)) {
+      isProjectNameValid = true;
+      this.projectInfo.projectName = this.projectName;
     }
-    downloadTemplate (){
-
+    if (!isProjectNameValid) {
+      this.projectPrompt.push(projectNamePrompt);
     }
+  }
+  isCwdEmpty() {
+    const localPath = process.cwd();
+    const fileList = fs.readdirSync(localPath);
+    return !!fileList?.length;
+  }
+  downloadTemplate() {}
 }
 
-function init (args) {
-    return new Create(args)
+function init(args) {
+  return new Create(args);
 }
-module.exports = init
+module.exports = init;
--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -1,101 +1,102 @@
-'use strict';
-const path = require('node:path')
-const log = require('@design-cli-dev/logs')
-const Package = require('@design-cli-dev/package')
-const cp = require('node:child_process')
+"use strict";
+const path = require("node:path");
+const log = require("@design-cli-dev/logs");
+const Package = require("@design-cli-dev/package");
+const cp = require("node:child_process");
+
 module.exports = exec;
 
-const PACKAGES_NAME= {
-    create: '@design-cli-dev/create',
-    publish: '@design-cli-dev/publish'
-}
-const CACHE_DIR = 'dependencies';
-async function exec () {
-    try {
-    const command = arguments[arguments.length-1]
-    const commandName = command.name()
-    const pkgName = PACKAGES_NAME[commandName]
-    let homePath = process.env.CLI_HOME_PATH
+const PACKAGES_NAME = {
+  create: "@design-cli-dev/create",
+  publish: "@design-cli-dev/publish",
+};
+const CACHE_DIR = "dependencies";
+async function exec() {
+  try {
+    const command = arguments[arguments.length - 1];
+    const commandName = command.name();
+    const pkgName = PACKAGES_NAME[commandName];
+    let homePath = process.env.CLI_HOME_PATH;
     // 指定定制包的路径
-    let targetPath = process.env.CLI_TARGET_PATH
+    let targetPath = process.env.CLI_TARGET_PATH;
     // 包的安装路径
-    let storePath = ''
-    const packageVersion = 'latest';
-    let pkg
+    let storePath = "";
+    const packageVersion = "latest";
+    let pkg;
     if (!targetPath) {
-        // 不指定的定制包路径话要读缓存的默认包，
-        // 先得到包
-        // 且更新到最新版本
-        targetPath = path.resolve(homePath, CACHE_DIR) // 缓存路径
-        storePath = path.resolve(homePath, CACHE_DIR, 'node_modules')
-        const options = {
-            targetPath,
-            storePath,
-            pkgName,
-            packageVersion
-        }
-    pkg = new Package(options)
-    log.verbose('pkgName',pkgName)
-    log.verbose('targetPath',targetPath)
-    log.verbose('storePath',storePath)
-    // 判断包是否存在
-    if (await pkg.exist()){
-        log.info('本地缓存存在包',pkg.cacheFilePath)
+      // 不指定的定制包路径话要读缓存的默认包，
+      // 先得到包
+      // 且更新到最新版本
+      targetPath = path.resolve(homePath, CACHE_DIR); // 缓存路径
+      storePath = path.resolve(homePath, CACHE_DIR, "node_modules");
+      const options = {
+        targetPath,
+        storePath,
+        pkgName,
+        packageVersion,
+      };
+      pkg = new Package(options);
+      log.verbose("pkgName", pkgName);
+      log.verbose("targetPath", targetPath);
+      log.verbose("storePath", storePath);
+      // 判断包是否存在
+      if (await pkg.exist()) {
+        log.info("本地缓存存在包", pkg.cacheFilePath);
         // 存在，更新
-        await pkg.update()
-    }else {
-        log.info('本地缓存查找不到初始化包',`准备安装${pkgName}`)
-        await pkg.install()
-    }
+        await pkg.update();
+      } else {
+        log.info("本地缓存查找不到初始化包", `准备安装${pkgName}`);
+        await pkg.install();
+      }
     } else {
-        // 指定了定制包的路径，就不需要管缓存。直接初始化
-        pkg = new Package({
-            targetPath,
-            pkgName,
-            packageVersion
-        })
+      // 指定了定制包的路径，就不需要管缓存。直接初始化
+      pkg = new Package({
+        targetPath,
+        pkgName,
+        packageVersion,
+      });
     }
     // 得到pkg包的入口文件,并调用
-    const rootFile = await pkg.getRootFilePath()
-    log.verbose('rootFile', rootFile)
-    if (rootFile){
-        // 优化：这里开启子进程去调用
-        const args = Array.from(arguments);
-        const cmd = args[args.length - 1];
-        const o = Object.create(null);
-        // 因为这里是执行子模块，所以把父级命令行的参数都去掉
-        Object.keys(cmd).forEach(key => {
-          if (cmd.hasOwnProperty(key) &&
-          !key.startsWith('_') &&
-            key !== 'parent') {
-            o[key] = cmd[key];
-          }
-        });
-        args[args.length - 1] = o;
-        const code = `require('${rootFile}').call(null,${JSON.stringify(args)})`
-        // node直接执行代码：node -e require(xxx)
-        const child = cp.spawn('node',['-e',code], {
-            // 子进程的工作目录
-            cwd: process.cwd(),
-            // inherit:通过相应的标准输入输出流到/从父进程
-            // 如果是ignore,就只能执行命令，不能输入输出
-            // pipe 的话，子进程只能读不能写，是单向的。
-            stdio: 'inherit',
-        })
-        child.on('error', e => {
-            log.error(e.message);
-            process.exit(1);
-        });
-        child.on('exit', e => {
-        log.verbose('命令执行成功:' + e);
+    const rootFile = await pkg.getRootFilePath();
+    log.verbose("rootFile", rootFile);
+    if (rootFile) {
+      // 优化：这里开启子进程去调用
+      const args = Array.from(arguments);
+      const cmd = args[args.length - 1];
+      const o = Object.create(null);
+      // 因为这里是执行子模块，所以把父级命令行的参数都去掉
+      Object.keys(cmd).forEach((key) => {
+        if (
+          cmd.hasOwnProperty(key) &&
+          !key.startsWith("_") &&
+          key !== "parent"
+        ) {
+          o[key] = cmd[key];
+        }
+      });
+      args[args.length - 1] = o;
+      const code = `require('${rootFile}').call(null,${JSON.stringify(args)})`;
+      // node直接执行代码：node -e require(xxx)
+      const child = cp.spawn("node", ["-e", code], {
+        // 子进程的工作目录
+        cwd: process.cwd(),
+        // inherit:通过相应的标准输入输出流到/从父进程
+        // 如果是ignore,就只能执行命令，不能输入输出
+        // pipe 的话，子进程只能读不能写，是单向的。
+        stdio: "inherit",
+      });
+      child.on("error", (e) => {
+        log.error(e.message);
+        process.exit(1);
+      });
+      child.on("exit", (e) => {
+        log.verbose("命令执行成功:" + e);
         process.exit(e);
-        });
-    }else{
-        log.error('找不到npm包的入口文件',`请检查包${pkgName}是否指定入口文件`)
+      });
+    } else {
+      log.error("找不到npm包的入口文件", `请检查包${pkgName}是否指定入口文件`);
     }
- 
-
-} catch(err){
-    throw new Error(err)
-}
+  } catch (err) {
+    throw new Error(err);
+  }
 }
